4e91b9985c4b53916c6c87acd14560398cd7675a
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-11-21 11:38:04 +0100

    Création du projet Java avec les dépendences

---
+++ ProduitCategorie/src/main/java/fr/bediss/ProduitCategorieApplication.java
@@ -0,0 +1,13 @@
+package fr.bediss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitCategorieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitCategorieApplication.class, args);
+	}
+
+}
